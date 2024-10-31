01e4c8cf2074b6fa45ffb67019a31c7d1a19ac1f
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-10-31 19:33:37 +0100

    refined network scanning
    + improved error handling
    + accelerated network-scan
    + added directory for storing handshake

--- dump.sh
+++ dump.sh
@@ -7,12 +7,18 @@ NC='\033[0m'
 # define print function
 function print() {
     local error=$1
-    shift
+    local message=$2
+    local carriage_return=${3:-false}
+
+    local prefix="["
+    if [ "$carriage_return" = true ]; then
+        prefix="\r\033[K["
+    fi
 
     if [ "$error" -eq 0 ]; then
-        echo -e "[${GREEN}OK${NC}] $@"
+        echo -e "$prefix${GREEN}OK${NC}] $message"
     else
-        echo -e "[${RED}OK${NC}] $@"
+        echo -e "$prefix${RED}ER${NC}] $message"
         exit 1
     fi
 }
@@ -44,34 +50,43 @@ print $? "network card"
 # check if monitor mode is enabled
 iwconfig "$interface" 2>/dev/null | grep -q "Mode:Monitor"
 if [ $? -ne 0 ]; then
+    printf "==> enabling monitor mode..."
     airmon-ng check kill >/dev/null
     airmon-ng start "$interface" >/dev/null
 fi
 
-print $? "monitor mode"
+print $? "monitor mode" true
 
 
-# set and create empty dump directory
+# set and create directories
 dump_dir="/tmp/${interface}.networkdump"
-mkdir -p "$dump_dir"
-rm -rf "$dump_dir/*"
+handshake_dir="/tmp/${interface}.handshake"
+for dir in "$dump_dir" "$handshake_dir"; do
+    mkdir -p "$dir"
+    rm -rf "$dir/*"
+done
 
-# scan for target network for 15 seconds
-sudo airodump-ng "$interface" --essid "$target" --write "$dump_dir/dump" >/dev/null 2>&1 &
-airodump_pid=$!
-sleep 15
-kill $airodump_pid
+# scan for target network 3 times for 5 seconds
+printf "==> scanning for '$target'..."
+scan_status=1
+for i in {1..3}; do
+    sudo airodump-ng "$interface" --essid "$target" --write "$dump_dir/dump" >/dev/null 2>&1 &
+    airodump_pid=$!
+    sleep 5
+    kill $airodump_pid
+    if grep -q "$target" "$dump_dir/dump-0$i.csv"; then
+        scan_status=0
+        break
+    fi
+done
 
-# evaluate scan and start handshake sniff
-if [ $? -eq 0 ]; then
-    network_data=$(grep "$target" "$dump_dir/dump-01.csv")
-    bssid=$(echo "$network_data" | awk -F, '{print $1}')
-    channel=$(echo "$network_data" | awk -F, '{gsub(/^ *| *$/, "", $4); print $4}')
+print $scan_status "target network found" true
 
-    print 0 "network dump ($bssid -> $channel)"
-    sleep 3
+network_data=$(grep "$target" "$dump_dir/dump-0$i.csv")
+bssid=$(echo "$network_data" | awk -F, '{print $1}')
+channel=$(echo "$network_data" | awk -F, '{gsub(/^ *| *$/, "", $4); print $4}')
 
-    airodump-ng -d "$bssid" -c "$channel" -w handshake "$interface"
-else
-    print 1 "network dump"
-fi
+print 0 "network dump ($bssid -> $channel)"
+sleep 3
+
+airodump-ng -d "$bssid" -c "$channel" -w "$handshake_dir" "$interface"
